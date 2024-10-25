2cf3bc841776b87a4fce02220ec957b8f961cc65
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-10-25 16:45:32 +0300

    Fixed time interval calculation

--- custom_components/domika/ha_event/__init__.py
+++ custom_components/domika/ha_event/__init__.py
@@ -6,7 +6,7 @@ import asyncio
 from typing import TYPE_CHECKING
 
 from ..const import LOGGER, PUSH_INTERVAL
-from ..ha_event import flow as ha_event_flow
+from . import flow as ha_event_flow
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
@@ -24,7 +24,7 @@ async def event_pusher(hass: HomeAssistant) -> None:
     LOGGER.debug("Event pusher started")
     try:
         while True:
-            await asyncio.sleep(PUSH_INTERVAL.seconds)
+            await asyncio.sleep(PUSH_INTERVAL.total_seconds())
             try:
                 await ha_event_flow.push_registered_events(hass)
             except Exception:  # noqa: BLE001
